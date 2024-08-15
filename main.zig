2827d2b86cb7d8d3d383c44769e4a3fd218f476c
wilhasse <willianhasse@gmail.com>
2024-08-15 08:46:20 -0300

    parse multiple lines from file

--- main.zig
+++ main.zig
@@ -54,48 +54,11 @@ pub fn main() !void {
     const file = try std.fs.cwd().openFileZ(std.os.argv[scriptArg], .{});
     defer file.close();
 
-    const file_size = try file.getEndPos();
-    const prog = try allocator.alloc(u8, file_size);
-
-    _ = try file.read(prog);
-
-    var tokens = std.ArrayList(lex.Token).init(allocator);
-    const lexErr = lex.lex(prog, &tokens);
-    if (lexErr) |err| {
-        std.debug.print("Failed to lex: {s}", .{err});
-        return;
-    }
-
-    if (debugTokens) {
-        for (tokens.items) |token| {
-            std.debug.print("Token: {s}\n", .{token.string()});
-        }
-    }
-
-    if (tokens.items.len == 0) {
-        std.debug.print("Program is empty", .{});
-        return;
-    }
-
-    const parser = parse.Parser.init(allocator);
-    var ast: parse.Parser.AST = undefined;
-    switch (parser.parse(tokens.items)) {
-        .err => |err| {
-            std.debug.print("Failed to parse: {s}", .{err});
-            return;
-        },
-        .val => |val| ast = val,
-    }
-
-    if (debugAST) {
-        ast.print();
-    }
-
     var db: RocksDB = undefined;
     const dataDirectory = std.mem.span(std.os.argv[databaseArg]);
     switch (RocksDB.open(allocator, dataDirectory)) {
         .err => |err| {
-            std.debug.print("Failed to open database: {s}", .{err});
+            std.debug.print("Failed to open database: {s}\n", .{err});
             return;
         },
         .val => |val| db = val,
@@ -103,42 +66,87 @@ pub fn main() !void {
     defer db.close();
 
     const storage = Storage.init(allocator, db);
-
     const executor = execute.Executor.init(allocator, storage);
-    switch (executor.execute(ast)) {
-        .err => |err| {
-            std.debug.print("Failed to execute: {s}", .{err});
-            return;
-        },
-        .val => |val| {
-            if (val.rows.len == 0) {
-                std.debug.print("ok\n", .{});
-                return;
-            }
 
-            std.debug.print("| ", .{});
-            for (val.fields) |field| {
-                std.debug.print("{s}\t\t|", .{field});
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+
+    var buf: [1024]u8 = undefined;
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        var tokens = std.ArrayList(lex.Token).init(allocator);
+        defer tokens.deinit();
+
+        // Create a buffer to hold the new string with newline
+        var line_n = try allocator.alloc(u8, line.len + 1);
+        defer allocator.free(line_n);
+
+        // Copy the original line to the new buffer
+        std.mem.copyForwards(u8, line_n, line);
+
+        // Append the newline character
+        line_n[line.len] = '\n';
+        const lexErr = lex.lex(line_n, &tokens);
+        if (lexErr) |err| {
+            std.debug.print("Failed to lex: {s}\n", .{err});
+            continue;
+        }
+
+        if (debugTokens) {
+            for (tokens.items) |token| {
+                std.debug.print("Token: {s}\n", .{token.string()});
             }
-            std.debug.print("\n", .{});
-            std.debug.print("+ ", .{});
-            for (val.fields) |field| {
-                var fieldLen = field.len;
-                while (fieldLen > 0) {
-                    std.debug.print("=", .{});
-                    fieldLen -= 1;
+        }
+
+        if (tokens.items.len == 0) {
+            std.debug.print("Empty line, skipping\n", .{});
+            continue;
+        }
+
+        const parser = parse.Parser.init(allocator);
+        var ast: parse.Parser.AST = undefined;
+        switch (parser.parse(tokens.items)) {
+            .err => |err| {
+                std.debug.print("Failed to parse: {s}\n", .{err});
+                continue;
+            },
+            .val => |val| ast = val,
+        }
+
+        if (debugAST) {
+            ast.print();
+        }
+
+        switch (executor.execute(ast)) {
+            .err => |err| {
+                std.debug.print("Failed to execute: {s}\n", .{err});
+            },
+            .val => |val| {
+                if (val.rows.len == 0) {
+                    std.debug.print("ok\n", .{});
+                    continue;
                 }
-                std.debug.print("\t\t+", .{});
-            }
-            std.debug.print("\n", .{});
-
-            for (val.rows) |row| {
                 std.debug.print("| ", .{});
-                for (row) |cell| {
-                    std.debug.print("{s}\t\t|", .{cell});
+                for (val.fields) |field| {
+                    std.debug.print("{s}\t\t|", .{field});
                 }
                 std.debug.print("\n", .{});
-            }
-        },
+                std.debug.print("+ ", .{});
+                for (val.fields) |field| {
+                    var fieldLen = field.len;
+                    while (fieldLen > 0) : (fieldLen -= 1) {
+                        std.debug.print("=", .{});
+                    }
+                    std.debug.print("\t\t+", .{});
+                }
+                std.debug.print("\n", .{});
+                for (val.rows) |row| {
+                    std.debug.print("| ", .{});
+                    for (row) |cell| {
+                        std.debug.print("{s}\t\t|", .{cell});
+                    }
+                    std.debug.print("\n", .{});
+                }
+            },
+        }
     }
 }
